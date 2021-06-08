d77899e9e61c3c74bdd0ed7129876bab72ff3540
SiyaAmonkar <71375897+SiyaAmonkar@users.noreply.github.com>
2021-06-08 15:47:45 +0530

    Argo and ArgoCD installation steps

--- install/setup.sh
+++ install/setup.sh
@@ -1,1 +1,32 @@
-rgr
+# argo workflow installation
+kubectl create ns argo
+kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo-workflows/stable/manifests/namespace-install.yaml 
+
+# argo cli
+# Download the binary
+curl -sLO https://github.com/argoproj/argo/releases/download/v3.1.0-rc11/argo-linux-amd64.gz
+
+# Unzip
+gunzip argo-linux-amd64.gz
+
+# Make binary executable
+chmod +x argo-linux-amd64
+
+# Move binary to path
+mv ./argo-linux-amd64 /usr/local/bin/argo
+
+# Test installation
+argo version
+
+# argocd
+kubectl create ns argocd
+curl -sSfL https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml | kubectl apply -n argocd -f -
+
+# argcd cli
+curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/v2.0.3/argocd-linux-amd64
+chmod +x /usr/local/bin/argocd
+
+# To access the api server 
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+
+pass=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
